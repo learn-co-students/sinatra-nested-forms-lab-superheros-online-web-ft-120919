821c7644903f38548163d1dab2933d001def6956
github username <github email address>
2020-02-01 13:30:37 -0800

    Done.

---
+++ app/models/members.rb
@@ -0,0 +1,18 @@
+class Members
+
+    attr_reader :name, :power, :bio
+
+    @@all = []
+
+    def initialize(args)
+        @name = args[:name]
+        @power = args[:power]
+        @bio = args[:bio]
+    end
+
+    def self.all
+        @@all
+    end
+
+
+end
