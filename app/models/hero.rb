489532c96640e0f56893df182e9bbb146b7b3bc3
Zach Cullen <zacullen@gmail.com>
2020-02-06 08:36:18 -0500

    Done.

---
+++ app/models/hero.rb
@@ -0,0 +1,24 @@
+class Hero
+  attr_accessor :name, :power, :biography
+
+  @@all = []
+
+  def initialize(attributes)
+    @name = attributes[:name]
+    @power = attributes[:power]
+    @biography = attributes[:biography]
+    self.save
+  end
+
+  def save
+    @@all << self
+  end
+
+  def all
+    @@all
+  end
+
+  def clear
+    @@all = []
+  end
+end
