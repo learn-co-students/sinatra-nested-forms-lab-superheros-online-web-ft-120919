824fcdc5fff0eb7ac97bd5d2d548cd598c34c4f6
mrarthurwhite <mister.arthur.white@gmail.com>
2020-02-07 23:12:25 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,31 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+
+        erb :team
+    end
+
+    post '/teams' do
+        @tn = params["team"]["name"]
+        @tm = params["team"]["motto"]
+
+        # hero 1
+        @h1n = params["team"]["heroes"][0]["name"]
+        @h1p = params["team"]["heroes"][0]["power"]
+        @h1b = params["team"]["heroes"][0]["bio"]
+
+        # hero 2
+        @h2n = params["team"]["heroes"][1]["name"]
+        @h2p = params["team"]["heroes"][1]["power"]
+        @h2b = params["team"]["heroes"][1]["bio"]
+
+        # hero 3
+        @h3n = params["team"]["heroes"][2]["name"]
+        @h3p = params["team"]["heroes"][2]["power"]
+        @h3b = params["team"]["heroes"][2]["bio"]
+
+        erb :super_hero
+    end
 
 end
